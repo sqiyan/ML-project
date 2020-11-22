f1387d0d3d3cb245f83341f9fb66c0a17e16c591
ongkahyuan <52844020+ongkahyuan@users.noreply.github.com>
2020-11-22 17:46:43 +0800

    add support for train and test data

--- HMM.py
+++ HMM.py
@@ -23,15 +23,16 @@ class HMM_script():
         self.open_file()
 
     def open_file(self):
-        self.data = np.genfromtxt(self.path + "/train",delimiter=" ",dtype = str, encoding="utf-8", invalid_raise = False)
+        self.train_data = np.genfromtxt(self.path + "/train",delimiter=" ",dtype = str, encoding="utf-8", invalid_raise = False)
+        self.test_data = np.genfromtxt(self.path + "/dev.in",delimiter=" ",dtype = str, encoding="utf-8", invalid_raise = False)
 
     def est_emission_params(self):
-        y_vals = self.data[:,1]
+        y_vals = self.train_data[:,1]
         count_y = {}
         count_y_to_x = {}
         self.e_x_given_y = {}
-        for i in range(self.data.shape[0]):
-            transition = self.data[i,:]
+        for i in range(self.train_data.shape[0]):
+            transition = self.train_data[i,:]
             transition = tuple(transition)
             if transition not in count_y_to_x:
                 count_y_to_x[transition] = 1
@@ -50,8 +51,29 @@ class HMM_script():
         for entry, count in count_y.items():
             self.e_x_given_y[("#UNK#",entry)] = 0.5/(count+0.5)
 
-        print(self.e_x_given_y)
+
+    def ymax_given_x(self):
+        self.est_emission_params()
+        x_max_prob = {}
+        self.y_max_given_x = {}  
+        for entry, prob in self.e_x_given_y.items():
+            if entry[0] not in x_max_prob:
+                x_max_prob[entry[0]] = prob
+                self.y_max_given_x[entry[0]] = entry[1]
+            else:
+                if x_max_prob[entry[0]]<prob:
+                    x_max_prob[entry[0]] = prob
+                    self.y_max_given_x[entry[0]] = entry[1]
+        
+
+    def evaluate_ymax(self):
+        self.y_max_given_x()
+        # f = open(self.path + "/dev.p2.out","w")
+        #     f.write("{} {}\n".format(x,y))
+        # f.close()
+
+
 
 
 hmm = HMM_script(args)
-hmm.est_emission_params()
+hmm.ymax_given_x()
